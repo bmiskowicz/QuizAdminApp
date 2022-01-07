f7cfdc8a219bc850cd26dad5ead0f822e933c0d9
xRei <miskowicz@interia.pl>
2022-01-07 21:26:39 +0100

    wersja ze statycznymi kategoriami

--- app/src/main/java/com/example/quizadminapp/CategoryActivity.java
+++ app/src/main/java/com/example/quizadminapp/CategoryActivity.java
@@ -1,21 +1,12 @@
 package com.example.quizadminapp;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
